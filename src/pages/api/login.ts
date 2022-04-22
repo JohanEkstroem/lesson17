1148078fa3a574c0553a00810f91dbecca3cfa2a
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-04-22 21:06:02 +0200

    Use Iron to seal (encrypt) cookie

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import Iron from '@hapi/iron';
 import Cookies from 'cookies';
 
 // INSECURE! Do not do this. Only for the sake of example
@@ -7,7 +8,12 @@ const USERS: Record<string, string> = {
   marcus: 'sosecret',
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export const ENC_KEY =
+  "C&#]F-?)3yvcK`^FKf(H[vL.#cDLm;yC9u?fcL^3{>@vVzg'@Dz%SM3k;<7!uc<G";
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method == 'POST') {
     const { username, password } = req.body;
 
@@ -15,10 +21,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       const cookies = new Cookies(req, res);
       cookies.set(
         'session',
-        JSON.stringify({
-          username: username,
-          loggedIn: true,
-        })
+        await Iron.seal(
+          { username: username, loggedIn: true },
+          ENC_KEY,
+          Iron.defaults
+        )
       );
       res.status(200).end();
     } else {
